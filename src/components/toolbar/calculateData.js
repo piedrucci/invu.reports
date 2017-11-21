6ab1b32b796829d263f950c32c95f968a48bfe2b
Roberth Mejias <piedrucci@gmail.com>
2017-11-21 10:53:12 -0500

    fixed sales report

--- src/components/toolbar/calculateData.js
+++ src/components/toolbar/calculateData.js
@@ -1,33 +1,16 @@
 import _ from 'lodash'
 
 export const ProcessSales = (groupName, data) => {
-// console.log(data);
   // agrupar por el campo seleccionado (groupName)
   let grouped = _.groupBy(data, (row)=> {
     return row.item[groupName]
   })
-  console.log(grouped)
 
   const arrayItems = _.map( grouped, (row) => {
     let rowInfo = row[0]
 
-    /*var totalMods = 0
-    // calcular el total en modificadores para el item actual
-    // if ( groupName === 'nombre' ){
-      totalMods = rowInfo.item.modif.reduce( (total, mod) => {
-        const totalMod = ( mod.hora === rowInfo.item.hora ) ? parseFloat(total) + parseFloat(mod.total) : 0
-
-        return totalMod
-      },0.0 )
-    // }
-
-    totalMods = ( totalMods!==0 ) ? (parseFloat(rowInfo.item.total_vendido)+parseFloat(totalMods)).toFixed(2) : (rowInfo.item.total_vendido).toFixed(2)
-
-    const discount =  (rowInfo.item.descuento).toFixed(2)
-    const net = ( parseFloat(totalMods) - discount ).toFixed(2)*/
-
     let itemInfo = {
-      item:rowInfo.item.nombre,
+      item: (groupName==='') ,
       category:rowInfo.item.nombrecat,
       quantityItems: 0,
       quantityOrders: 0,
@@ -37,37 +20,28 @@ export const ProcessSales = (groupName, data) => {
       orderTax: 0
     }
 
-    // agrupar por otro campo que no sea nombre
-    if ( row.length>1 ) {
-      // itemInfo.item = "------"
-      // if ( row.item.nombrecat === "PIZZAS SPECIALS" ){
-        // console.log(row);
-      // }
-      row.map( (item, index) => {
-        //if (index>0){
-          let totalMods = 0
-          totalMods = item.item.modif.reduce( (total, mod) => {
-            const totalMod = ( mod.hora === rowInfo.item.hora ) ? parseFloat(total) + parseFloat(mod.total) : 0
-            return totalMod
-           },0.0 )
+    row.map( (item, index) => {
 
-          itemInfo.quantityItems = parseInt(itemInfo.quantityItems, 10) + parseInt(item.item.cantidad_vendida, 10)
-          itemInfo.quantityOrders = parseInt(itemInfo.quantityOrders, 10) + parseInt(item.item.cantidad_ordenes, 10)
+      let totalMods = 0
+      totalMods = item.item.modif.reduce( (total, mod) => {
+        const totalMod = ( mod.hora === rowInfo.item.hora ) ? parseFloat(total) + parseFloat(mod.total) : 0
+        return totalMod
+      },0.0 )
 
-          const itemGross = ( parseFloat(itemInfo.gross) + parseFloat(item.item.total_vendido) ).toFixed(2)
-          itemInfo.gross = parseFloat(itemGross) + totalMods
+      itemInfo.quantityItems = parseInt(itemInfo.quantityItems, 10) + parseInt(item.item.cantidad_vendida, 10)
+      itemInfo.quantityOrders = parseInt(itemInfo.quantityOrders, 10) + parseInt(item.item.cantidad_ordenes, 10)
 
-          const sds = (parseFloat( itemInfo.discount ) + parseFloat( item.item.descuento )).toFixed(2)
-          itemInfo.discount = parseFloat(sds)
+      const grossRow =  parseFloat(item.item.total_vendido) + totalMods
+      itemInfo.gross =  parseFloat( ( parseFloat(itemInfo.gross) + grossRow).toFixed(2) )
 
-          const itemNet = ( parseFloat(itemInfo.net) + parseFloat( (parseFloat(item.item.total_vendido) - parseFloat(item.item.descuento)).toFixed(2) ) ).toFixed(2)
-          itemInfo.net = parseFloat( itemNet )
-          itemInfo.orderTax = parseFloat(itemInfo.orderTax) + parseFloat( (item.item.tax).toFixed(2) )
+      const sds = (parseFloat( itemInfo.discount ) + parseFloat( item.item.descuento )).toFixed(2)
+      itemInfo.discount = parseFloat(sds)
 
+      const itemNet = ( parseFloat(itemInfo.net) + ( grossRow - parseFloat(item.item.descuento)) ).toFixed(2)
+      itemInfo.net = parseFloat( itemNet )
+      itemInfo.orderTax = parseFloat(itemInfo.orderTax) + parseFloat( (item.item.tax).toFixed(2) )
 
-        //}
-      } )
-    }
+    } )
 
     return itemInfo
 
